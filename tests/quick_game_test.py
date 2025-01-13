233fd48cb7e0b5a1229f7920478549b08079dc2d
Alisa Vazieva <vazievaar@answer-42.ru>
2025-01-13 14:51:25 +0500

    Added allure

--- tests/quick_game_test.py
+++ tests/quick_game_test.py
@@ -1,19 +1,22 @@
+import allure
 from pages.quick_game_page import QuickGamePage
 from playwright.sync_api import expect, Page
 
 
+@allure.feature('Quick game')
+@allure.story('Start quick game and check results')
 def test_quick_game(page: Page):
-    quick_game = QuickGamePage(page)
-    # Close game rules
-    quick_game.close_rules()
-    expect(quick_game.close_rules_btn, "Правила игры не были закрыты").not_to_be_visible()
-    # Start game
-    quick_game.start_game()
-    # Insert text
-    quick_game.insert_text()
-    # Check results
-    quick_game.get_game_result()
-
-
-
-
+    with allure.step('Go to quick game'):
+        quick_game = QuickGamePage(page)
+        # Close game rules
+        quick_game.close_rules()
+        expect(quick_game.close_rules_btn, "Правила игры не были закрыты").not_to_be_visible()
+    with allure.step('Start game'):
+        # Start game
+        quick_game.start_game()
+    with allure.step('Insert text'):
+        # Insert text
+        quick_game.insert_text()
+    with allure.step('Check results'):
+        # Check results
+        quick_game.get_game_result()
